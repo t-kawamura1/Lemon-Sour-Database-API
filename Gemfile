8b51c4c16f1a89a3c3c28bb27481f1a7a70694d0
tkawamura <tkawamura.stayhome@gmail.com>
2021-07-13 15:30:08 +0900

    add: devise-token-auth & registrations_controller

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'carrierwave', '~> 2.0'
 gem 'mini_magick'
 gem 'fog-aws'
 gem 'dotenv-rails'
+gem 'devise'
+gem 'devise_token_auth'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+        private
+
+        def sign_up_params
+          params.permit(:name, :email, :password)
+        end
+
+        def account_update_params
+          params.permit(:name, :email)
+        end
+      end
+    end
+  end
+end
+
