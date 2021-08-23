03691e517c5bd08abb9ede5ff18b47ac0cb261db
tkawamura <tkawamura.stayhome@gmail.com>
2021-08-23 10:47:34 +0900

    fix: env var & mailer_sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: ENV['API_URL'] }
+  config.action_mailer.default_url_options = { host: ENV['FRONT_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
